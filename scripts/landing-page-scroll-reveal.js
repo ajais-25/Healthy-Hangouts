91e690ca50628ef2c230501bc2177ed555ac5b7d
Swapnamoy-git <swapnamoymidya@gmail.com>
2023-08-22 22:55:41 +0530

    Vision section added for landing page

---
+++ scripts/landing-page-scroll-reveal.js
@@ -0,0 +1,8 @@
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2000,
+    delay:200
+  });
+   ScrollReveal().reveal('.vision-heading', { delay: 100, origin: 'left' });
+   ScrollReveal().reveal('.vision-normal-text', { delay: 200, origin: 'bottom' });
