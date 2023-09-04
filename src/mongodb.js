5990b692a95da458f33347c85cccfbe8a0916741
Deepjyoti Das <deepjyoti2301@gmail.com>
2023-09-04 14:23:12 +0530

    enhanced security and console overhaul

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,40 +1,40 @@
-const mongoose=require("mongoose")
-require('dotenv').config();
+const mongoose = require("mongoose");
+const colors = require("colors");
+require("dotenv").config();
 
 const MONGO_URI = process.env.MONGO_URI;
-mongoose.connect(MONGO_URI)
-.then(()=>{
-    console.log("mongodb connected huehue");
-})
-.catch(()=>{
-    console.log('failed to connect huehue');
-})
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("mongodb connected huehue".green);
+  })
+  .catch(() => {
+    console.log("failed to connect huehue".red);
+  });
 
-const logInSchema=new mongoose.Schema({
-    firstname:{
-        type:String,
-        required:true
-    },
-    lastname:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    confirmpassword:{
-        type:String,
-        required:true
-    }
+const logInSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  confirmpassword: {
+    type: String,
+    required: true,
+  },
+});
 
+const collection = new mongoose.model("LogInCollection", logInSchema);
 
-})
-
-const collection=new mongoose.model('LogInCollection',logInSchema)
-
-module.exports=collection;
+module.exports = collection;
